e4667b5c13b867200978df28161d906ede533bb1
yueyongyue <yueyongyue@sina.cn>
2020-04-28 17:50:44 +0800

    modify: User isito client.go

--- common/access/access.go
+++ common/access/access.go
@@ -1,61 +1,19 @@
 package access
 
 import (
-	"kingfisher/kf/common/access"
-	"kingfisher/kf/common/log"
-	authenticationv1alpha1 "kingfisher/king-istio/pkg/client/clientset/versioned/typed/authentication/v1alpha1"
-	configv1alpha2 "kingfisher/king-istio/pkg/client/clientset/versioned/typed/config/v1alpha2"
-	networkingv1alpha3 "kingfisher/king-istio/pkg/client/clientset/versioned/typed/networking/v1alpha3"
-	securityv1beta1 "kingfisher/king-istio/pkg/client/clientset/versioned/typed/security/v1beta1"
+	"github.com/open-kingfisher/king-utils/common/access"
+	"github.com/open-kingfisher/king-utils/common/log"
+	versionedclient "istio.io/client-go/pkg/clientset/versioned"
 )
 
-func IstioNetworkingClient(clusterId string) (*networkingv1alpha3.NetworkingV1alpha3Client, error) {
+func IstioClient(clusterId string) (*versionedclient.Clientset, error) {
 	config, err := access.GetConfig(clusterId)
 	if err != nil {
 		return nil, err
 	}
-	clientSet, err := networkingv1alpha3.NewForConfig(config)
+	clientSet, err := versionedclient.NewForConfig(config)
 	if err != nil {
-		log.Errorf("Istio Networking clientSet error: %s", err)
-		return nil, err
-	}
-	return clientSet, err
-}
-
-func IstioAuthenticationClient(clusterId string) (*authenticationv1alpha1.AuthenticationV1alpha1Client, error) {
-	config, err := access.GetConfig(clusterId)
-	if err != nil {
-		return nil, err
-	}
-	clientSet, err := authenticationv1alpha1.NewForConfig(config)
-	if err != nil {
-		log.Errorf("Istio Authentication clientSet error: %s", err)
-		return nil, err
-	}
-	return clientSet, err
-}
-
-func IstioSecurityClient(clusterId string) (*securityv1beta1.SecurityV1beta1Client, error) {
-	config, err := access.GetConfig(clusterId)
-	if err != nil {
-		return nil, err
-	}
-	clientSet, err := securityv1beta1.NewForConfig(config)
-	if err != nil {
-		log.Errorf("Istio Security clientSet error: %s", err)
-		return nil, err
-	}
-	return clientSet, err
-}
-
-func IstioConfigClient(clusterId string) (*configv1alpha2.ConfigV1alpha2Client, error) {
-	config, err := access.GetConfig(clusterId)
-	if err != nil {
-		return nil, err
-	}
-	clientSet, err := configv1alpha2.NewForConfig(config)
-	if err != nil {
-		log.Errorf("Istio Config clientSet error: %s", err)
+		log.Errorf("Istio clientSet error: %s", err)
 		return nil, err
 	}
 	return clientSet, err
