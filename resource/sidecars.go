0d4ed27c78d8accd95dbbb02d7458a744ef1c310
yueyongyue <yueyongyue@sina.cn>
2020-04-28 16:17:23 +0800

    Modify: User isito client.go

--- resource/sidecars.go
+++ resource/sidecars.go
@@ -1,28 +1,30 @@
 package resource
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/open-kingfisher/king-utils/common"
+	"github.com/open-kingfisher/king-utils/common/handle"
+	"github.com/open-kingfisher/king-utils/common/log"
+	"github.com/open-kingfisher/king-utils/kit"
+	"istio.io/client-go/pkg/apis/networking/v1alpha3"
+	versionedclient "istio.io/client-go/pkg/clientset/versioned"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
-	"kingfisher/kf/common"
-	"kingfisher/kf/common/handle"
-	"kingfisher/kf/common/log"
-	"kingfisher/kf/kit"
-	"kingfisher/king-istio/pkg/apis/networking/v1alpha3"
-	networkingv1alpha3 "kingfisher/king-istio/pkg/client/clientset/versioned/typed/networking/v1alpha3"
 )
 
 type SidecarsResource struct {
 	Params   *handle.Resources
 	PostData *v1alpha3.Sidecar
-	Access   *networkingv1alpha3.NetworkingV1alpha3Client
+	Access   *versionedclient.Clientset
 }
 
 func (r *SidecarsResource) Get() (*v1alpha3.Sidecar, error) {
-	i, err := r.Access.Sidecars(r.Params.Namespace).Get(r.Params.Name, metav1.GetOptions{})
+	var ctx context.Context
+	i, err := r.Access.NetworkingV1alpha3().Sidecars(r.Params.Namespace).Get(ctx, r.Params.Name, metav1.GetOptions{})
 	if err == nil {
 		i.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{Kind: "Sidecar", Version: "networking.istio.io/v1alpha3"})
 	}
@@ -30,11 +32,13 @@ func (r *SidecarsResource) Get() (*v1alpha3.Sidecar, error) {
 }
 
 func (r *SidecarsResource) List() (*v1alpha3.SidecarList, error) {
-	return r.Access.Sidecars(r.Params.Namespace).List(metav1.ListOptions{})
+	var ctx context.Context
+	return r.Access.NetworkingV1alpha3().Sidecars(r.Params.Namespace).List(ctx, metav1.ListOptions{})
 }
 
 func (r *SidecarsResource) Delete() (err error) {
-	if err = r.Access.Sidecars(r.Params.Namespace).Delete(r.Params.Name, &metav1.DeleteOptions{}); err != nil {
+	var ctx context.Context
+	if err = r.Access.NetworkingV1alpha3().Sidecars(r.Params.Namespace).Delete(ctx, r.Params.Name, metav1.DeleteOptions{}); err != nil {
 		return
 	}
 	auditLog := handle.AuditLog{
@@ -50,11 +54,12 @@ func (r *SidecarsResource) Delete() (err error) {
 }
 
 func (r *SidecarsResource) Patch() (res *v1alpha3.Sidecar, err error) {
+	var ctx context.Context
 	var data []byte
 	if data, err = json.Marshal(r.Params.PatchData.Patches); err != nil {
 		return
 	}
-	if res, err = r.Access.Sidecars(r.Params.Namespace).Patch(r.Params.Name, types.JSONPatchType, data); err != nil {
+	if res, err = r.Access.NetworkingV1alpha3().Sidecars(r.Params.Namespace).Patch(ctx, r.Params.Name, types.JSONPatchType, data, metav1.PatchOptions{}); err != nil {
 		log.Errorf("%s patch error:%s; Json:%+v; Name:%s", common.Sidecars, err, string(data), r.Params.Name)
 		return
 	}
@@ -71,7 +76,8 @@ func (r *SidecarsResource) Patch() (res *v1alpha3.Sidecar, err error) {
 }
 
 func (r *SidecarsResource) Update() (res *v1alpha3.Sidecar, err error) {
-	if res, err = r.Access.Sidecars(r.Params.Namespace).Update(r.PostData); err != nil {
+	var ctx context.Context
+	if res, err = r.Access.NetworkingV1alpha3().Sidecars(r.Params.Namespace).Update(ctx, r.PostData, metav1.UpdateOptions{}); err != nil {
 		log.Errorf("%s update error:%s; Json:%+v; Name:%s", common.Sidecars, err, r.PostData, r.PostData.Name)
 		return
 	}
@@ -90,7 +96,8 @@ func (r *SidecarsResource) Update() (res *v1alpha3.Sidecar, err error) {
 }
 
 func (r *SidecarsResource) Create() (res *v1alpha3.Sidecar, err error) {
-	if res, err = r.Access.Sidecars(r.Params.Namespace).Create(r.PostData); err != nil {
+	var ctx context.Context
+	if res, err = r.Access.NetworkingV1alpha3().Sidecars(r.Params.Namespace).Create(ctx, r.PostData, metav1.CreateOptions{}); err != nil {
 		log.Errorf("%s create error:%s; Json:%+v; Name:%s", common.Sidecars, err, r.PostData, r.PostData.Name)
 		return
 	}
